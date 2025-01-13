28bbf40c51839ea94c453ac9e6d7b3f8de076324
심우석 <dntjr5741@naver.com>
2025-01-14 03:04:50 +0900

    feat : todo, done 없을때 조건 추가

--- src/components/getItem.tsx
+++ src/components/getItem.tsx
@@ -45,32 +45,44 @@ export default function GetItem({ todoList, updateTodoList }: GetItemProps) {
         <div className="mb-5 mt-7">
           <img src="/Image/todo.svg" alt="todo" />
         </div>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id} className="flex pt-1.5 pb-1.5 mb-2 ">
-              <button
-                className="flex items-center text-black flex-1 p-3 rounded-full border-2 border-black bg-[#F1F5F9]"
-                style={{
-                  boxShadow: "3px 3px 1px black",
-                }}
-              >
-                <img
-                  src="/Image/circle.svg"
-                  alt="Icon"
-                  className="w-10 h-10 mr-5"
-                  onClick={() => handleToggle(todo)}
-                />
-                <div
-                  onClick={() =>
-                    goToDetail(todo.id, todo.isCompleted, todo.name)
-                  }
+        {todos.length > 0 ? (
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id} className="flex pt-1.5 pb-1.5 mb-2 ">
+                <button
+                  className="flex items-center text-black flex-1 p-3 rounded-full border-2 border-black bg-[#F1F5F9]"
+                  style={{
+                    boxShadow: "3px 3px 1px black",
+                  }}
                 >
-                  {todo.name}
-                </div>
-              </button>
-            </li>
-          ))}
-        </ul>
+                  <img
+                    src="/Image/circle.svg"
+                    alt="Icon"
+                    className="w-10 h-10 mr-5"
+                    onClick={() => handleToggle(todo)}
+                  />
+                  <div
+                    onClick={() =>
+                      goToDetail(todo.id, todo.isCompleted, todo.name)
+                    }
+                  >
+                    {todo.name}
+                  </div>
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>
+            <div className="flex justify-center items-center mt-[60px]">
+              <img src="/Image/nothingDoneLarge.svg" alt="Nothing to do" />
+            </div>
+            <div className="flex justify-center items-center text-center text-gray-400">
+              할 일이 없어요.
+              <br /> TODO를 새롭게 추가해주세요!
+            </div>
+          </div>
+        )}
       </div>
 
       {/* Done 리스트 */}
@@ -78,34 +90,45 @@ export default function GetItem({ todoList, updateTodoList }: GetItemProps) {
         <div className="mb-5 mt-7">
           <img src="/Image/done.svg" alt="todo" />
         </div>
-
-        <ul>
-          {dones.map((todo) => (
-            <li key={todo.id} className="flex pt-1.5 pb-1.5 mb-2">
-              <button
-                className="flex items-center text-black flex-1 p-3 rounded-full border-2 border-black bg-[#EDE9FE]"
-                style={{
-                  boxShadow: "3px 3px 1px black",
-                }}
-              >
-                <img
-                  src="/Image/Property 1=Frame 2610233.svg"
-                  alt="Icon"
-                  className="w-10 h-10 mr-5"
-                  onClick={() => handleToggle(todo)}
-                />
-                <div
-                  onClick={() =>
-                    goToDetail(todo.id, todo.isCompleted, todo.name)
-                  }
-                  className="line-through"
+        {dones.length > 0 ? (
+          <ul>
+            {dones.map((todo) => (
+              <li key={todo.id} className="flex pt-1.5 pb-1.5 mb-2">
+                <button
+                  className="flex items-center text-black flex-1 p-3 rounded-full border-2 border-black bg-[#EDE9FE]"
+                  style={{
+                    boxShadow: "3px 3px 1px black",
+                  }}
                 >
-                  {todo.name}
-                </div>
-              </button>
-            </li>
-          ))}
-        </ul>
+                  <img
+                    src="/Image/Property 1=Frame 2610233.svg"
+                    alt="Icon"
+                    className="w-10 h-10 mr-5"
+                    onClick={() => handleToggle(todo)}
+                  />
+                  <div
+                    onClick={() =>
+                      goToDetail(todo.id, todo.isCompleted, todo.name)
+                    }
+                    className="line-through"
+                  >
+                    {todo.name}
+                  </div>
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>
+            <div className="flex justify-center items-center mt-[60px]">
+              <img src="/Image/nothingLarge.svg" alt="Nothing" />
+            </div>
+            <div className="mt-[20px] flex justify-center items-center text-center text-gray-400">
+              아직 다 한 일이 없어요.
+              <br /> 해야 할 일을 체크해보세요!
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
